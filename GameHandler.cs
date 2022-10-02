123cf1d71aa28ea191b046bbc7316367feceb2dd
wiszou <luis.santiago44.ls@gmail.com>
2022-10-02 19:53:07 +0800

    Update GameHandler.cs

--- GameHandler.cs
+++ GameHandler.cs
@@ -7,7 +7,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        JETH
     }
 
     // Update is called once per frame
